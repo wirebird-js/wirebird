b965f6809eff0c33b91bc6b936e88001a267bb18
no <no>
2019-08-31 22:44:04 +0300

    todo

--- pages/api/updates.ts
+++ pages/api/updates.ts
@@ -3,6 +3,7 @@ import { ContextedRequest } from '../../server/ContextedRequest';
 import { LoggerEvent } from 'http-inspector';
 
 const post = (req: NextApiRequest & ContextedRequest, res: NextApiResponse) => {
+    //TODO: validate using JSON schema
     const socketServer = req.context.socketServer;
     const body = req.body as LoggerEvent;
     socketServer.broadcastLoggerEvent(body);
