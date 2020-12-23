3e035ccbced264555ce3dec84b50c843f487c5e1
no <no>
2020-12-23 21:44:18 +0200

    fixes #11

--- components/content-view/TextView.tsx
+++ components/content-view/TextView.tsx
@@ -1,9 +1,20 @@
+import { makeStyles } from '@material-ui/core';
 import { FC } from 'react';
+
+const useStyles = makeStyles(() => ({
+    pre: {
+        wordBreak: 'break-all',
+        whiteSpace: 'pre-wrap',
+        fontFamily: 'monospace', //TODO: add font
+        fontSize: 13,
+    },
+}));
 
 export interface ITextViewProps {
     data: Buffer;
 }
 
 export const TextView: FC<ITextViewProps> = ({ data }) => {
-    return <pre>{data.toString('utf8')}</pre>;
+    const classes = useStyles();
+    return <div className={classes.pre}>{data.toString('utf8')}</div>;
 };
