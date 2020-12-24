45a88e5f32a1523e4c7ef2c433aba0798b9dc810
no <no>
2020-12-24 14:19:00 +0200

    fixes #24

---
+++ server/argv.ts
@@ -0,0 +1,21 @@
+import yargs from 'yargs';
+
+export interface Argv {
+    headless: boolean;
+    port: number;
+}
+
+export const argv = (): Argv =>
+    yargs.env().options({
+        headless: {
+            alias: 'H',
+            type: 'boolean',
+            default: false,
+            describe: 'Do not open browser',
+        },
+        port: {
+            alias: 'p',
+            type: 'number',
+            default: 4380,
+        },
+    }).argv;
--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,23 @@
-import { join } from 'path';
+import chalk from 'chalk';
 import { createServer, IncomingMessage, RequestListener } from 'http';
-import { parse } from 'url';
+import { address as getMyIP } from 'ip';
 import next from 'next';
-import SocketServer from './SocketServer';
+import opener from 'opener';
+import { join } from 'path';
+import { parse } from 'url';
+import { argv } from './argv';
 import { ContextedRequest } from './ContextedRequest';
-
-const PORT = 4380;
+import SocketServer from './SocketServer';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const qrterm = require('qrcode-terminal');
 
 export default async function main({
     production,
 }: {
     production: boolean;
 }): Promise<void> {
+    const { headless, port } = argv();
+
     process.title = 'http-inspector-ui';
     const dev = !production;
     const app = next({ dev, dir: join(__dirname, '..') });
@@ -29,11 +35,25 @@ export default async function main({
             socketServer,
         };
         handle(req, res, parsedUrl);
-    }) as RequestListener).listen(PORT);
+    }) as RequestListener).listen(port);
 
     const socketServer = new SocketServer({ server });
+    const ip = getMyIP('public');
 
-    console.log(`Server listening at http://localhost:${PORT}`);
+    const listenURL = `http://0.0.0.0:${port}`;
+    const externalURL = `http://${ip}:${port}`;
+    const localURL = `http://localhost:${port}`;
+
+    qrterm.generate(externalURL);
+    console.log('Use this QR-code to open on a mobile device\n');
+
+    console.log(chalk`Listening at        : {bold ${listenURL}}`);
+    console.log(chalk`Open on your machine: {bold ${localURL}}`);
+    console.log(chalk`Open from your LAN  : {bold ${externalURL}}`);
+
+    if (!headless) {
+        opener(localURL);
+    }
 }
 
 if (require.main === module) {
